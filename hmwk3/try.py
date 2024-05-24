8d0e79bab40501d13a328134fa9c95233b2c1b0e
misak <xiangyu_yao0925@163.com>
2024-05-24 16:47:11 +0800

    5/24

--- hmwk3/try.py
+++ hmwk3/try.py
@@ -1,16 +1,26 @@
 import os
 import numpy as np
+import cv2 as cv
 path="hmwk3/omniglot-py/images_background"
 
 p= os.walk(path)
 paths=[]
-for i,(filepath,dirnames,filenames) in enumerate(p):
-    if(i==200):
+
+count=0
+for filepath,dirnames,filenames in p:
+    if len(filenames)!=20:
+        continue
+    if(count==200):
         break
+    count+=1
     for filename in filenames:
         paths.append(os.path.join(filepath,filename))
-        
-print(paths[:10])
+        label=filename.split('_')[0]
+
+print(paths[-1])
 print(len(paths))
-    
-    
+print(count)
+
+img=cv.imread(paths[-1])
+print(img.shape)
+
